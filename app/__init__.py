fb89ba3135f995044cfbc337ca0dfa70228870f9
judahsen <sardiusjudah@gmail.com>
2024-04-30 14:48:44 -0500

    new  update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+from config import Config
 from resources.post import routes
 from resources.user import routes
+from .models import db
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app,db)
+
+app.register_blueprint(api)
